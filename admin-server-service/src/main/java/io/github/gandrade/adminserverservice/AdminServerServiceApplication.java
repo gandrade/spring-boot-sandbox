34e2ad55fcc04f64fea47274766b545b7fcb88d7
Andrade, Gustavo (ESI) <gustavo.andrade@adp.com>
2018-08-27 20:02:37 -0300

    Adding Admin server console to monitor incoming Spring Boot applications

---
+++ admin-server-service/src/main/java/io/github/gandrade/adminserverservice/AdminServerServiceApplication.java
@@ -0,0 +1,14 @@
+package io.github.gandrade.adminserverservice;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerServiceApplication.class, args);
+	}
+}
